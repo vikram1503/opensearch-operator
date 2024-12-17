df5265d6933558c4058c81c58fca602e157d10e9
vikram1503 <vikram15031989@gmail.com>
2024-12-17 12:21:37 +0530

    test

--- 02-sg/variables.tf
+++ 02-sg/variables.tf
@@ -17,36 +17,4 @@ variable "common_tags" {
   
 }
 
-variable "vpn_sg_rules" {
-     default = [
-        {
-        from_port = 943
-        to_port = 943
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        },
-        {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        },
-        {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        },
-        {
-        from_port = 1194
-        to_port = 1194
-        protocol = "udp"
-        cidr_blocks = ["0.0.0.0/0"]
-        }
-     ] 
-}
 
-variable "db_sg_description" {
-    default = "SG for DB MySQL Instances"
-
-}
