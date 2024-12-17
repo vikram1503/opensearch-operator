2b10fe3f72283fd036fddbf1a1e5b0260e427e0b
vikram1503 <vikram15031989@gmail.com>
2024-12-17 10:47:45 +0530

    test

---
+++ 03-bastion/variables.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+    default = "OpenSearch"
+  
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "common_tags" {
+    default = {
+        Project = "OpenSearch"
+        Environment = "dev"
+        Terraform = "true"
+    }
+  
+}
