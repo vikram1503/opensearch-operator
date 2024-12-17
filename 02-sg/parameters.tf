4abd34ee6b51164747cb2ddd5fd161ac8180f19e
vikram1503 <vikram15031989@gmail.com>
2024-12-17 12:18:52 +0530

    test

--- 02-sg/parameters.tf
+++ 02-sg/parameters.tf
@@ -1,19 +1,7 @@
-resource "aws_ssm_parameter" "db_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/db_sg_id"
-  type  = "String"
-  value = module.db.sg_id
-}
-
 resource "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
   type  = "String"
   value = module.bastion.sg_id
-}
-
-resource "aws_ssm_parameter" "vpn_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/vpn_sg_id"
-  type  = "String"
-  value = module.vpn.sg_id
 }
 
 resource "aws_ssm_parameter" "cluster_sg_id" {
